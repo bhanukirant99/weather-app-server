6c0349f07045b25048eea3544036ac5caf57309f
bhanukirant99 <bhanukirant99@gmail.com>
2020-08-10 22:27:24 +0530

    adding public folder

--- src/app.js
+++ src/app.js
@@ -1,21 +1,39 @@
+const path = require('path');
 const exprexx = require('express');
 
 const app = exprexx();
+const publicDir = path.join(__dirname, '../public');
 
-app.get('', (req, res) => {
-    res.send('Home page');
-});
+app.use(exprexx.static(publicDir));
 
-app.get('/help', (req, res) => {
-    res.send('Help page');
-});
+// app.get('/help', (req, res) => {
+//     res.send([{
+//         name: 'Raj',
+//         age: 12
+//     },{
+//         name: 'Sam',
+//         age: 13
+//     }]);
+// });
 
-app.get('/about', (req, res) => {
-    res.send('About page');
-});
+// app.get('/about', (req, res) => {
+//     res.send('<h1>About</h1>');
+// });
 
 app.get('/weather', (req, res) => {
-    res.send('Weather page');
+    res.send({
+        place_name: "Whoopie, вул. Пушкінська, 31, Одеса, Odessa, Ukraine",
+        center: [
+        30.742241,
+        46.477855
+        ],
+        geometry: {
+            coordinates: [
+                30.742241,
+                46.477855
+            ]
+        }
+    });
 });
 
 
